d8a7678f118bafa36b4541a2771fb4b00ad440da
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-23 19:55:26 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import product_image from "./assets/images/image-product-desktop.jpg";
+import logo_cart from "./assets/images/icon-cart.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-full h-screen flex justify-center items-center">
+      <div className="sm:w-[570px] sm:h-[450px] mx-6 w-full h-[700px] bg-slate-300 flex rounded-md flex-col sm:flex-row">
+        <div className="sm:flex-1 h-2/5 p-[30px] w-full sm:h-full bg-cover bg-center rounded-l-lg" style={{ backgroundImage: `url(${product_image})` }}>
+        </div>
+        <div className="sm:flex-1 h-3/5 sm:h-full p-[30px] bg-white flex flex-col justify-between rounded-tr-lg">
+          <h2 className=" text-grayish-blue tracking-[5px] text-sm">PERFUME</h2>
+          <h1 className="font-fraunces text-3xl text-dark-blue">Gabrielle Essence Eau De Parfum</h1>
+          <p className="text-grayish-blue text-sm ">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div className="flex items-center ">
+            <h2 className="font-fraunces text-3xl mr-4 text-dark-cyan">$149.99</h2>
+            <h3 className="text-grayish-blue text-sm">$169.99</h3>
+          </div>
+          <button className="text-sm rounded-lg bg-dark-cyan p-[15px] hover:bg-hover-cyan text-white flex justify-center font-bold">
+            <span className="mr-[10px]"><img src={logo_cart} alt="logo_cart" /></span>
+            Add to Cart
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fraunces: ['Fraunces', 'sans-serif'],
+      },
+      backgroundColor: {
+        'grayish-blue': 'hsl(228, 12%, 48%)',
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'hover-cyan': 'hsl(158, 38%, 25%)',
+        'dark-blue': 'hsl(212, 21%, 14%)',
+      },
+      textColor: {
+        'grayish-blue': 'hsl(228, 12%, 48%)',
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'dark-blue': 'hsl(212, 21%, 14%)',
+      },
+    },
   },
   plugins: [],
 }
